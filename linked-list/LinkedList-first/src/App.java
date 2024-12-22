444a01405ac860c07827ef52389b01ea263839e3
mayhrem <renecruz.0711@gmail.com>
2024-12-21 22:30:50 -0600

    Adding append and remove last methods

--- linked-list/LinkedList-first/src/App.java
+++ linked-list/LinkedList-first/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        LinkedList list = new LinkedList();
+        list.append(4);
+        list.append(10);
+        list.append(5);
+        list.append(0);
+        list.print();
+        list.removeLast();
+        list.print();
     }
 }
--- linked-list/LinkedList-first/src/LinkedList.java
+++ linked-list/LinkedList-first/src/LinkedList.java
@@ -24,5 +24,65 @@ public class LinkedList {
     this.length = 0;
   }
 
+  // ? Create a new Node to add to the Linked List
+  //? Check if the Linked List is not empty
+  // ? In case the Linked List is not empty
+  // ? Create temp file to point the tail node to temp and then use the pointers
+  // ? new node points to temp
+  // ? temp.next points to new node
+  // ? tail points to new node
+  public void append (int value) {
+    Node node = new Node(value);
+    //? Check if the list is empty 
+    if (length == 0) {
+      head = node;
+      tail = node;
+    } else {
+      tail.next = node;
+      tail = node;
+    }
+    length++;
+  } 
+
+  // ? Check if the list is not empty
+  // ? If the list is empty -> return null
+  // ? Create nodes -> before and current
+  // ? If the list has only one element -> point head and tail to null and remove element 
+  // ? If the list has more than one element 
+  public Node removeLast () {
+    if (length == 0) return null;
+    Node current = head;
+    Node before = head;
+
+    //? Traverse the linked list until until the end of the list
+    while (current.next != null) {
+      before = current;
+      current = current.next;
+    }
+
+    //? Check if the list has only one element
+    if (current == head) {
+      head = null;
+      tail = null; 
+    } else {
+      before.next = null;
+      tail = before;
+    }
+    length--;
+    return current;
+  }
+
+  public void print () {
+    Node current = head;
+    System.out.println("Linked List - elements ("+length+")");
+    while (current != null) {
+      System.out.println("Value -->"+current.value);
+      current = current.next;
+    }
+  }
+
+  // ? Adding the element to the fist of the list
+  //? Point current 
+  public Node prepend (int value) {}
   
 }
